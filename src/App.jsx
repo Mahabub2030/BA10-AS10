c29a34b752074448f36c5c299456e2c26b1bd9e7
mahabub <mahabubalom72091@gmail.com>
2024-12-18 11:01:31 +0300

    code all added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h3>hellow world</h3>
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,21 @@
+
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+
+const Main = () => {
+    return (
+        <div className="container mx-auto">
+         
+            <Navbar></Navbar>
+            
+            <div className="py-12">               
+                      
+            </div>
+
+            <Footer></Footer>
+
+        </div>
+    );
+};
+
+export default Main;
